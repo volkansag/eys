e5e1ebb52a37dc9f45ecd1f448b2500d0bc80035
Volkan Sağ <volkansagmobile@gmail.com>
2026-01-30 19:10:03 +0300

    Veritabanı problemleri çözüldü.

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from "../../prisma/generated/prisma/client";
+import { PrismaClient } from "~~/prisma/generated/prisma/client";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 const prismaClientSingleton = () => {
   const adapter = new PrismaBetterSqlite3({
-    url: process.env.DATABASE_URL ?? "file:./db.sqlite",
+    url: process.env.DATABASE_URL ?? "file:prisma/db.sqlite",
   });
   return new PrismaClient({ adapter });
 };
